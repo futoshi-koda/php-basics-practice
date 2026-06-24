09240571b40b03b45850316b64884ceff47e8c9c
futoshi-koda <fkoda0310@outlook.com>
2026-06-24 18:33:40 +0900

    add unit

--- src/7-1-6_hands-on/practice/price_calculator.php
+++ src/7-1-6_hands-on/practice/price_calculator.php
@@ -11,24 +11,24 @@
     ?>
 <br>
     <?php echo "単価:";
-          echo number_format($price,0, '.', ',');
+          echo number_format($price,0, '.', ',')."円";
     ?>
 <br>
     <?php echo "数量:";
-          echo number_format($quantity,0, '.', ',');
+          echo number_format($quantity,0, '.', ',')."個";
     ?>
 <br>
     <?php echo "小計:";
-          echo number_format($price*$quantity,0, '.', ',');
+          echo number_format($price*$quantity,0, '.', ',')."円";
     ?>
 <br>
     <?php echo "消費税(10%):";
-          echo number_format($price*$quantity*$tax_rate,0,'.', ',');
+          echo number_format($price*$quantity*$tax_rate,0,'.', ',')."円";
     ?>
 <br>
 <strong>
     <?php echo  "合計金額:";
-          echo  number_format( $price*$quantity+$price*$quantity*$tax_rate,0,'.', ',');
+          echo  number_format( $price*$quantity+$price*$quantity*$tax_rate,0,'.', ',')."円";
     ?>
 </strong>
 </p>
